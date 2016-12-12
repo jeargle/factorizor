13191078baffb37df3e2e8631967a36e619eb7d9
John Eargle <jeargle@gmail.com>
2016-12-11 18:51:03 -0600

    #1 got initial version of factor wheel up and running

--- js/factorizor.js
+++ js/factorizor.js
@@ -58,7 +58,7 @@ titleState = {
 playState = {
     create: function() {
         'use strict';
-        var block, i, enemy, textStyle, text;
+        var block, i, enemy, textStyle, text, primeStyle;
 
         this.keyboard = game.input.keyboard;
 
@@ -80,7 +80,7 @@ playState = {
         this.bullets.setAll('checkWorldBounds', true);
         
         this.bulletTime = 0;
-        this.bulletTimeOffset = 300;
+        this.bulletTimeOffset = 200;
         this.bulletSpeed = 500;
 
         // this.fireButton = this.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
@@ -104,6 +104,29 @@ playState = {
                                  i+1, textStyle);
             text.anchor.set(0.5);
         }
+
+        // Factor wheel
+        this.primes = [2, 3, 5, 7, 11, 13, 17, 19];
+        primeStyle = {
+            font: '20px Arial',
+            fill: '#ffffff',
+            align: 'center',
+        };
+        this.wheel = [];
+        for (i=0; i<this.primes.length; i++) {
+            this.wheel.push(game.add.text(400, 450,
+                                          this.primes[i],
+                                          primeStyle));
+            this.wheel[i].anchor.set(0.5);
+        }
+        this.wheel[3].y = 475;
+        this.wheel[2].y = 500;
+        this.wheel[1].y = 525;
+        this.wheel[0].y = 550;
+        this.chosenPrime = this.primes[2];   // active prime
+        this.tweenPrimes = [0, 1, 2, 3, 4];
+        this.primeTime = 0;
+        this.primeTimeOffset = 200;
         
         // Controls
         this.cursors = game.input.keyboard.addKeys({
@@ -131,9 +154,75 @@ playState = {
             // console.log('angle: ' + this.gun.angle);
         }
         
+        if (game.time.now > this.primeTime) {
+            if (this.cursors.up.isDown) {
+                this.primeSelect('up');
+            }
+            else if (this.cursors.down.isDown) {
+                this.primeSelect('down');
+            }
+        }
+        
         if (this.cursors.fire.isDown) {
             this.fire();
         }
+    },
+    /**
+     * Move prime factor wheel up or down.
+     * @param scrollDir {string} - direction to move: 'up' or 'down'
+     */
+    primeSelect: function(scrollDir) {
+        'use strict';
+        var tween, prime, i;
+
+        if (scrollDir === 'up') {
+            this.primeTime = game.time.now +
+                this.primeTimeOffset;
+            for (i=0; i<4; i++) {
+                prime = this.wheel[this.tweenPrimes[i]];
+                tween = game.add.tween(prime)
+                    .to({y: prime.y-25}, 200,
+                        Phaser.Easing.Linear.None,
+                        true);
+            }
+            
+            for (i=4; i>0; i--) {
+                this.tweenPrimes[i] = this.tweenPrimes[i-1];
+            }
+            
+            if (this.tweenPrimes[0] === 0) {
+                this.tweenPrimes[0] = this.primes.length-1;
+            }
+            else {
+                this.tweenPrimes[0] = this.tweenPrimes[0]-1;
+            }
+            this.wheel[this.tweenPrimes[0]].y = 550;
+        }
+        else {
+            this.primeTime = game.time.now +
+                this.primeTimeOffset;
+            for (i=1; i<5; i++) {
+                prime = this.wheel[this.tweenPrimes[i]];
+                tween = game.add.tween(prime)
+                    .to({y: prime.y+25}, 200,
+                        Phaser.Easing.Linear.None,
+                        true);
+            }
+            
+            for (i=0; i<4; i++) {
+                this.tweenPrimes[i] = this.tweenPrimes[i+1];
+            }
+            
+            if (this.tweenPrimes[4] === this.primes.length-1) {
+                this.tweenPrimes[4] = 0;
+            }
+            else {
+                this.tweenPrimes[4] = this.tweenPrimes[4]+1;
+            }
+            this.wheel[this.tweenPrimes[4]].y = 450;
+        }
+        this.chosenPrime = this.primes[this.tweenPrimes[2]];
+        console.log(this.chosenPrime);
     },
     fire: function() {
         'use strict';
